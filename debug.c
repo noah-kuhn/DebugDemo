c25fb5c5ec66669a353e054fe7a0d8cf69f878b7
nk_ultra <kuhnnoah01@gmail.com>
2021-02-01 21:58:48 -0600

    added some includes and an empty main function in debug.c

---
+++ debug.c
@@ -0,0 +1,16 @@
+/*
+ *  Authors: Thomas Peavler and Noah Kuhn
+ *
+ */
+
+/* Here are some fairly standard libraries */
+#include <stdlib.h>     /* standard library */
+#include <stdio.h>      /* standard input/output library */
+#include <string.h>     /* standard string library */
+
+#include "debug.h"      /* we also need to include our header file! this includes stdbool for us */
+
+/* this is similar to Java main: this is the actual function that executes */
+int main() {
+    return 0; /* returning 0 from main usually means everything went smoothly! */
+}
---
+++ debug.h
@@ -0,0 +1,9 @@
+/*
+ *  Authors: Thomas Peavler and Noah Kuhn
+ *
+ */
+
+/* This line includes the standard boolean library - C doesn't have booleans as a primitive, so we
+   have to bring them in with a standard header file */
+#include <stdbool.h> 
+
