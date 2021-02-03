45c15ec78129e460e7bfe75e823a05876ced2f62
Thomas Peavler <thomasp@cs.utexas.edu>
2021-02-03 10:30:01 -0600

    Added demonstration of defensive programming with strings on the stack.

--- debug.c
+++ debug.c
@@ -19,6 +19,7 @@ struct node
 
 int function1();
 struct node *recursive(struct node *next_node);
+void defensiveProgramming();
 
 /* this is similar to Java main: this is the actual function that executes */
 int main() {
@@ -37,6 +38,7 @@ int main() {
     temp->next = NULL;
 
     function1();
+    defensiveProgramming();
     recursive(root);
 
     // ---------------- This is where the bugs start -------------------
@@ -95,6 +97,40 @@ struct node *recursive(struct node *next_node)
         int useless = next_node->a + next_node->b;
         return recursive(next_node->next);
     }
+}
+
+void defensiveProgramming()
+{
+    char string[8];
+    char string2[8];
+
+    char *first_string = "********1234567";
+    char *second_string = "abcdefg";
+
+    // Copy a string with out a limit
+    strcpy(string2, second_string);
+    printf("string2 = %s\n", string2);
+
+    strcpy(string, first_string);
+    printf("string2 = %s\n\n", string2);
+
+    // NEVER COPY A STRING WITH strcpy!
+
+    strncpy(string2, second_string, 8);
+    printf("string2 = %s\n", string2);
+
+    strncpy(string, first_string, 8);
+    printf("string2 = %s\n\n", string2);
+
+    // But how does the computer know when a string ends?
+    char *new_string1 = "12345678";
+    strncpy(string, new_string1, 8);
+
+    printf("string1 = %s\n", string);
+
+    // We didn't leave room for the null character '\0'
+    string[7] = '\0';
+    printf("finalString = %s\n", string);
 }
 
 
